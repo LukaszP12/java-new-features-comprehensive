971508d829d48d372b166a3c7a912288baaf244e
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-21 18:03:36 +0200

    private methods() in interfaces since Java 9

---
+++ var-keyword/src/main/java/pl/piwowarski/java9/privateInterfaces/Demo1.java
@@ -0,0 +1,9 @@
+package pl.piwowarski.java9.privateInterfaces;
+
+class Demo1 {
+    public static void main(String[] args) {
+        MyInterfaceImpl myInterface = new MyInterfaceImpl();
+        myInterface.method1();
+        myInterface.method2();
+    }
+}
