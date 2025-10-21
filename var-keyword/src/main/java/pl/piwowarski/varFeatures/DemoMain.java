e007210fd41d6837e1c35de03add015fca0dda8e
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-21 17:46:21 +0200

    -Must be initialized-You must assign a value at declaration time / Local scope only-Works only inside methods, constructors, and blocks / Not allowed for fields-You can't use var for class instance variables / Type is inferred at compile-time / Must be clear - The inferred type must be unambiguous

---
+++ var-keyword/src/main/java/pl/piwowarski/varFeatures/DemoMain.java
@@ -0,0 +1,29 @@
+package pl.piwowarski.varFeatures;
+
+import java.util.ArrayList;
+
+public class DemoMain {
+
+    // var keyword was introduced in Java 10
+
+    public static void main(String[] args) {
+        var message = "Hello, Java";
+        var number = 42;
+
+        System.out.println(message + "num: " + number);
+
+        var list = new ArrayList<String>();
+
+        list.stream().forEach(System.out::println);
+
+
+        var longNumber = 30L;
+        System.out.println(longNumber);
+
+//        var nullExample = null; var cannot be null
+//        public var add(var a, var b) {  // ❌ Not allowed
+//            return a + b;  // it cannot be neither method parameter or return type
+//        }
+    
+    }
+}
---
+++ var-keyword/src/main/java/pl/piwowarski/varFeatures/MapDemo.java
@@ -0,0 +1,18 @@
+package pl.piwowarski.varFeatures;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class MapDemo {
+    public static void main(String[] args) {
+//        Good for readability when the type is obvious:
+        var map = new HashMap<String, Integer>();
+        map.put("1", 10);
+        map.put("2", 20);
+        map.put("3", 30);
+
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        System.out.println("entries " + entries);
+    }
+}
